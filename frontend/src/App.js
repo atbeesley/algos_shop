6cd7add7e375de6af535e7860ce4d3e02625e3e4
atbeesley <alexander.beesley@gmail.com>
2021-07-11 18:19:27 +0100

    header & footer components working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 const App = () => {
   return (
     <>
-    <h1>Hi from Algos</h1>
+    <Header/>
+    <main>
+      <h1>Hi from Algos</h1>
+    </main>
+    <Footer/>
     </>
   );
 }
