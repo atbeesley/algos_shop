2741d23bcd20aeed9ac7ad64d0c1dc232188c6cf
atbeesley <alexander.beesley@gmail.com>
2021-07-13 08:10:19 +0100

    header looking good

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-
+import { Nav, Navbar, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header>
-            this is the header
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/home">Algos</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <Nav.Link href="/basket">Basket</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
