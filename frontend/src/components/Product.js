79390080f43230641cd87df19d9a60f8fd6fee8b
atbeesley <alexander.beesley@gmail.com>
2021-07-15 07:57:33 +0100

    Product details working

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,8 +4,20 @@ import { Card } from 'react-bootstrap'
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}></a>
-            <Card.Img src={product.image} variant='top' />
+                <a href={`/product/${product._id}`}></a>
+                <Card.Img src={product.image} variant='top' />
+            <Card.Body>
+                <a href={`/product/${product._id}`}></a>
+                <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
+                <Card.Text as='div'>
+                    <div className='my-3'>
+                        {product.rating} from {product.numReviews} reviews
+                    </div>
+                </Card.Text>
+                <Card.Text as='h3'>
+                    ${product.price}
+                </Card.Text>
+            </Card.Body>
         </Card>
     )
 }
