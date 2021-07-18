6950d6d671ed4d55f08608c98e7f6a59ae8b7f16
atbeesley <alexander.beesley@gmail.com>
2021-07-18 12:28:25 +0100

    ProductScreen-name, rating, & description working

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -14,9 +14,26 @@ const ProductScreen = ({ match }) => {
             <Link className="btn btn-light my-3" to='/'>Go back</Link>
             <Row>
                 <Col md={6}>
-                    <Image src={product.image} alt={product.name} />
+                    <Image src={product.image} alt={product.name} fluid />
                 </Col>
                 <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>{product.name}</h2>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating 
+                                value={product.rating} 
+                                text={`${product.numReviews} reviews`} 
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: £{product.price}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            {product.description}
+                        </ListGroup.Item>
+                        </ListGroup>
                 </Col>
             </Row>
         </>
