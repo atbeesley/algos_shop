62383c455b6eb984cf88f30107d4d621a7926dcb
atbeesley <alexander.beesley@gmail.com>
2021-07-18 10:33:27 +0100

    /product/ working dynamically by :id

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,11 +5,13 @@ import Rating from './Rating'
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-                <a href={`/product/${product._id}`}></a>
+                <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
+                </a>
             <Card.Body>
-                <a href={`/product/${product._id}`}></a>
+                <a href={`/product/${product._id}`}>
                 <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
+                </a>
                 <Card.Text as='div'>
                     <Rating 
                         value={product.rating}
