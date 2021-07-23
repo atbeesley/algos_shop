914184a1f255343426e96058fe9d85789a859b82
atbeesley <alexander.beesley@gmail.com>
2021-07-23 07:59:33 +0100

    adds colors to backend logs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
+
 
 const connectDB = async () => {
     try {
@@ -7,9 +9,9 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useCreateIndex: true
         })
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline)
     } catch(error){
-        console.error(`Error: ${error.message}`)
+        console.error(`Error: ${error.message}`.red.underline.bold)
         process.exit(1)
     }
 }
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
+
 
 dotenv.config()
 connectDB()
@@ -24,5 +26,5 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
         PORT, 
-        console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+        console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
         )
