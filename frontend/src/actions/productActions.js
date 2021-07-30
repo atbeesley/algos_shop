5ce0dcf3cd24abd13dd0120d20049ee859e6de4f
atbeesley <alexander.beesley@gmail.com>
2021-07-30 09:50:24 +0100

    error handling & loading... for homeScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -10,7 +10,7 @@ export const listProducts = () => async (dispatch) => {
         dispatch({ type: PRODUCT_LIST_REQUEST })
         const { data } = await axios.get('/api/products')
         dispatch({
-            type: PRODUCT_LIST_SUCCESS
+            type: PRODUCT_LIST_SUCCESS,
             payload: data
         })
     } catch (error) {
