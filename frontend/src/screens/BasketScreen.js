0fddf6c7f17eea2b5481aa1ca8ad42e0aa0eabee
atbeesley <alexander.beesley@gmail.com>
2021-08-07 23:08:43 +0100

    basketScreen functionality working better

--- frontend/src/screens/BasketScreen.js
+++ frontend/src/screens/BasketScreen.js
@@ -23,6 +23,10 @@ const BasketScreen = ({ match, location, history }) => {
         console.log('remove')
     }
 
+    const checkoutHandler = (id) => {
+        console.log('going to checkout')
+    }
+
     useEffect(() => {
         if(productId){
             dispatch(addToBasket(productId, qty))
@@ -30,14 +34,16 @@ const BasketScreen = ({ match, location, history }) => {
     }, [dispatch, productId, qty])
 
     return (
+        <Row>
        <Col md={8}>
            <h1>Shopping Basket</h1>
-           {basketItems.length === 0 ? 
+           {basketItems.length === 0 ? (
            <Message>
                Your basket is empty! 
                <br />
                <Link to='/'>Go back</Link>
-            </Message> : (
+            </Message>
+             ) : (
             <ListGroup variant='flush'>
                 {basketItems.map(item => (
                     <ListGroup.Item key={item.product}>
@@ -45,34 +51,52 @@ const BasketScreen = ({ match, location, history }) => {
                             <Col md={2}>
                                 <Image src={item.image} alt={item.name} fluid rounded />
                             </Col>
-                            <Col>
+                            <Col md={3}>
                                 <Link to={`/product/${item.product}`}>{item.name}</Link>
                             </Col>
                             <Col md={2}>
                                 {item.price}
                             </Col>
                             <Col md={2}>
-                                <Form.Control 
-                                    as='select' 
-                                    value={item.qty} 
-                                    onChange={(e) => dispatch(addToBasket(item.product, Number(e.target.value)))}>
-                                        {[...Array(item.countInStock).keys()].map(x => (
-                                            <option key={x + 1} value={x + 1}>
-                                                {x + 1}
-                                            </option>
-                                        ))}
-                                </Form.Control>
-                            </Col>
-                            <Col>
-                             <Button type='button' variant='light' onClick={removeFromBasketHandler(item.handler)}>
-                                 <i className='fas fa-trash'></i>
-                             </Button>
+                                <Button 
+                                      type='button' 
+                                      variant='light' 
+                                      onClick={() => removeFromBasketHandler(item.handler)}
+                                   >
+                                       <i className='fas fa-trash'></i>
+                                </Button>
                             </Col>
                         </Row>
-                    </ListGroup.Item>
+                        </ListGroup.Item>
                 ))}
-            </ListGroup>)}
-       </Col>
+                </ListGroup>
+             )}
+             </Col>
+                            <Col md={4}>
+                                      <Card>
+                                          <ListGroup variant='flush'>
+                                                  <ListGroup.Item>
+                                                      <h2>
+                                                          Subtotal ({basketItems.reduce((acculumator, item) => acculumator + item.qty, 0)}) items
+                                                      </h2>
+                                                      ${basketItems.reduce((acculumator, item) => acculumator + item.qty * item.price, 0
+                                                      ).toFixed(2)}
+                                                  </ListGroup.Item>
+                                                  <ListGroup.Item>
+                                                      <Button 
+                                                          type='button' 
+                                                          className='btn-block' 
+                                                          disabled={basketItems.length === 0}
+                                                          onClick={checkoutHandler}
+                                                      >
+                                                      Proceed to checkout!
+                                                      </Button>
+                                                  </ListGroup.Item>
+                                          </ListGroup>
+                                      </Card>
+                                  </Col>
+                            </Row>
+
     )
 }
 
