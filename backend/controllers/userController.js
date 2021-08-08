153dbb3a4aa0c1de5a883f907009823938f56a2f
atbeesley <alexander.beesley@gmail.com>
2021-08-08 17:21:11 +0100

    backend user auth working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,28 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
+// @desc Auth user & get token 
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    
+    const user = await User.findOne({ email })
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+    }
+})
+
 // @desc Fetch all users
 // @route GET /api/users
 // @access Public
@@ -22,4 +44,4 @@ const getUserById = asyncHandler(async (req, res) => {
     }
 })
 
-export { getUsers, getUserById }
+export { getUsers, getUserById, authUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ import express from 'express'
 const router = express.Router()
 import { 
     getUsers,
-    getUserById 
+    getUserById,
+    authUser
 } from '../controllers/userController.js'
 
 router.route('/').get(getUsers)
 router.route('/:id').get(getUserById)
+router.post('/login', authUser)
 
 export default router
