4b72391c3078654d431d869db82e573a81f779d3
atbeesley <alexander.beesley@gmail.com>
2021-08-08 12:48:22 +0100

    refactor users w/ controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,30 +1,11 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import User from '../models/userModel.js'
+import { 
+    getUsers,
+    getUserById 
+} from '../controllers/userController.js'
 
-// @desc Fetch all users
-// @route GET /api/users
-// @access Public
-router.get('/', 
-    asyncHandler(async (req, res) => {
-    const users = await User.find({})
-    res.json(users)
-}))
-
-// @desc Fetch all products
-// @route GET /api/products/:id
-// @access Public
-router.get(
-    '/:id', 
-    asyncHandler(async (req, res) => {
-    const user = await User.findById(req.params.id)
-    if(user){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('User not found')
-    }
-}))
+router.route('/').get(getUsers)
+router.route('/:id').get(getUserById)
 
 export default router
