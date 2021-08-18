742eca859b6cf4b20be72ff8eef0de0bed374301
atbeesley <alexander.beesley@gmail.com>
2021-08-18 17:15:08 +0100

    adds login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import BasketScreen from './screens/BasketScreen'
+import LoginScreen from './screens/LoginScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './bootstrap.min.css'
 import { Container } from 'react-bootstrap'
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/' component={HomeScreen} exact />
         <Route path='/product/:id' component={ProductScreen} />
         <Route path='/basket/:id?' component={BasketScreen} />
+        <Route path='/login' component={LoginScreen} />
       </Container>
     </main>
     <Footer />
