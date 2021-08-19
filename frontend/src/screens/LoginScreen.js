d52021043b1a66e7073ae15045b044fec5059ff2
atbeesley <alexander.beesley@gmail.com>
2021-08-19 18:24:03 +0100

    login fail & success workinggit add .

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -4,20 +4,35 @@ import { Form, Col, Row, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { Login } from '../actions/userActions'
+import { login } from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
 
-export const LoginScreen = ({ location }) => {
+const LoginScreen = ({ location, history }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const { loading, error, userInfo } = userLogin
+
     const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    useEffect(() => {
+        if(userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch(login(email, password))
     }
 
     return (
         <FormContainer>
             <h1>Sign in</h1>
+            {error && <Message variant={'danger'}>{error}</Message>}
+            {loading && <Loader />}
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='email'>
                     <Form.Label>Email Address</Form.Label>
