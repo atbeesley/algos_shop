f4e8c6e44bf1c7ce9160ad7c0eda0c307aa75f73
atbeesley <alexander.beesley@gmail.com>
2021-08-20 17:28:21 +0100

    adds user constants & reducers to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { basketReducer } from './reducers/basketReducers'
-import { userLoginReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     basket: basketReducer,
-    userLogin: userLoginReducer
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer
 })
 const basketItemsFromStorage = localStorage.getItem('basketItems') ? 
     JSON.parse(localStorage.getItem('basketItems')) : []
