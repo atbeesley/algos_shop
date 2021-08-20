c4fc3629480ad8693fa50807672ce4a17adf87f5
atbeesley <alexander.beesley@gmail.com>
2021-08-20 17:17:28 +0100

    user logout working

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
 
     const dispatch = useDispatch()
 
-    const userLogin = useSelector(state => state.userLogin)
+    const userLogin = useSelector((state) => state.userLogin)
     const { userInfo } = userLogin 
 
     const logoutHandler = () => {
-        console.log('logout')
+        dispatch(logout())
     }    
 
     return (
