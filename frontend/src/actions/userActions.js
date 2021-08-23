4182ebe8424f56fcf45160cf91f233d86c928486
atbeesley <alexander.beesley@gmail.com>
2021-08-23 08:16:47 +0100

    logout refreshes nicely

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -38,6 +38,7 @@ export const login = (email, password) => async(dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
+    window.location.reload()
 } 
 
 export const register = (name, email, password) => async(dispatch) => {
