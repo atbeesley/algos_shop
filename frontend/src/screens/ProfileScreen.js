f4d82d8c56acf6e88fd5fddd63444f39a058e52f
atbeesley <alexander.beesley@gmail.com>
2021-09-05 08:20:22 +0100

    fix navbar prof name edit bug

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
     const [name, setName] = useState('')
@@ -27,14 +28,15 @@ const ProfileScreen = ({ location, history }) => {
         if(!userInfo) {
             history.push('/login')
         } else {
-            if(!user.name){
+            if(!user || !user.name || success){
+                dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
             } else {
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
@@ -49,10 +51,10 @@ const ProfileScreen = ({ location, history }) => {
         <Row>
             <Col md={3}>
             <h2>User Profile</h2>
+            {loading && <Loader />}
             {message && <Message variant='danger'>{message}</Message>}
             {error && <Message variant='danger'>{error}</Message>}
             {success && <Message variant='success'>Changes saved</Message>}
-            {loading && <Loader />}
             <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
                     <Form.Label>Name</Form.Label>
