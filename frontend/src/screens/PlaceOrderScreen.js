098e95839bd71c8a4e26e0aeda34fbe654bc6ead
atbeesley <alexander.beesley@gmail.com>
2021-09-12 11:32:44 +0100

    shipping address working from state in PlaceOrder

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Form, Button, Col, ListGroup, Image, Card, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from './../components/Message'
+import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 const PlaceOrderScreen = () => {
+    const basket = useSelector(state => state.basket) 
+
     return (
         <>
-            Place yr order
+         <CheckoutSteps step1 step2 step3 step4 />
+         <Row>
+             <Col md={8}>
+                 <ListGroup variant="flush">
+                     <ListGroup.Item>
+                         <h2>Shipping</h2>
+                         <p>
+                             <strong>Address: </strong>
+                             {basket.shippingAddress.address},{" "}
+                             {basket.shippingAddress.city}{" "}
+                             {basket.shippingAddress.postcode},{" "}
+                             {basket.shippingAddress.country}
+                         </p>
+                     </ListGroup.Item>
+                 </ListGroup>
+             </Col>
+         </Row>
         </>
     )
 }
