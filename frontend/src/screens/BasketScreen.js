7711afc72c0c6de3835b656eeeb1502bcfc9f590
atbeesley <alexander.beesley@gmail.com>
2021-10-08 11:19:20 +0100

    basket text change

--- frontend/src/screens/BasketScreen.js
+++ frontend/src/screens/BasketScreen.js
@@ -37,7 +37,7 @@ const BasketScreen = ({ match, location, history }) => {
     return (
         <Row>
        <Col md={8}>
-           <h1>Shopping Basket</h1>
+           <h1>Your Basket</h1>
            {basketItems.length === 0 ? (
            <Message>
                Your basket is empty! 
