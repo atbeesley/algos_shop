a972923851f13cc2478b37cf142b7c159d9f60b7
atbeesley <alexander.beesley@gmail.com>
2021-11-12 12:11:42 +0000

    adds isPaid & isDelivered msgs to order scrn (+small html changes)

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -36,19 +36,28 @@ useEffect(() => {
                  <ListGroup variant="flush">
                      <ListGroup.Item>
                          <h2>Shipping</h2>
-                         <p><strong>Name: </strong>{order.user.name}</p>
-                         <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+                         <p>
+                             <strong>Name: </strong>{order.user.name}
+                         </p>
+                         <p>
+                            <strong>Email: </strong><a href={`mailto:${order.user.email}`}>{order.user.email}</a>
+                         </p>
                          <p>
                              <strong>Address: </strong>
                              {order.shippingAddress.address},{" "}{order.shippingAddress.city}{" "}
                              {order.shippingAddress.postcode},{" "}
                              {order.shippingAddress.country}
                          </p>
+                         {order.isDelivered ? <Message variant="success">Paid on {order.deliveredAt}</Message> : <Message variant="danger">Not delivered yet</Message>}
+
                      </ListGroup.Item>
                      <ListGroup.Item>
                          <h2>Payment Method</h2>
+                         <p>
                          <strong>Method: </strong>
                          {order.paymentMethod}
+                         </p>
+                         {order.isPaid ? <Message variant="success">Paid on {order.paidAt}</Message> : <Message variant="danger">Not paid</Message>}
                      </ListGroup.Item>
                      <ListGroup.Item>
                          <h2>Order Items</h2>
